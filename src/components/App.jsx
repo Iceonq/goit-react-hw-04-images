fcc2079340d9f3ef3a92fdd4d7de9ea8292dc332
Iceonq <bartek.sosnowicz@gmail.com>
2023-04-13 20:14:07 +0200

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import '../styles.css';
 import Button from './Button/Button';
 import ImageGallery from './ImageGallery/ImageGallery';
